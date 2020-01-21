8f236f7c73ec3c08fc60dc8584fd290f7c100bf6
okkursevi4 <okkursevi4@UC5PC9>
2020-01-21 20:12:46 +0100

    Log IN

--- TuristickaAgencija/src/dao/LoginDAO.java
+++ TuristickaAgencija/src/dao/LoginDAO.java
@@ -1,5 +1,37 @@
 package dao;
+import java.util.List;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import model.User;
 
 public class LoginDAO {
+	
+	SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
+	public User vratiUseraLogIn(String userName, String password) {
+		
+		
+		User user = new User();
+	
+		Query query = factory.getCurrentSession().createQuery("From User where userName :x AND password = :y");
+		query.setParameter("x", userName);
+		query.setParameter("y", password);
+		
+		Session sesija = factory.openSession();
+	
+			List<User> useri = query.getResultList();
+			if(useri.isEmpty()) {
+				return null;
+			}else if (useri.size()==1) {
+				return useri.get(0);	
+			} else {
+				return null; 
+			}
+			
+	}
 }
+
+
--- TuristickaAgencija/src/service/LoginService.java
+++ TuristickaAgencija/src/service/LoginService.java
@@ -1,5 +1,17 @@
 package service;
 
+import javax.transaction.Transactional;
+
+import dao.LoginDAO;
+import model.User;
+
 public class LoginService {
+	
+	LoginDAO dao = new LoginDAO();
+@Transactional
+	public User vratiUseraLogIn(String userName, String password) {
+		
+		return dao.vratiUseraLogIn(userName, password);
+	}
 
 }
